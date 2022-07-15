396061077b5281e5ff74d302147889f11f4908de
Queen Cobhams <queencobham@gmail.com>
2022-07-15 00:13:40 -0700

    new changes

--- script.js
+++ script.js
@@ -2,34 +2,45 @@
  let submitBtn = document.querySelector('.submit-btn')
  let list = document.querySelector('.lists')
 
-submitBtn.addEventListener('click', (e) =>{
+  submitBtn.addEventListener('click', (e) =>{
     e.preventDefault()
-    // alert('am going to be okay')
-    let todoItems = document.createElement('div')
-    todoItems.className = ('todo-items')
+
+    if(todoInput.value.trim().length<1){
+      alert('Please input text')
+    }else{
+      let todoItems = document.createElement('div')
+      todoItems.className = ('todo-items')
     
+      //completed todo items
+    let completedItems = document.createElement('button')
+    completedItems.innerHTML = '&#10003'
+    completedItems.className = ('completed-btn')
+    todoItems.appendChild(completedItems)
+   
     //new todo items
     let listItems = document.createElement('li')
     listItems.innerText = todoInput.value
     listItems.className = ('list-items')
     todoItems.appendChild(listItems)
-  
-    //completed todo items
-    let completedItems = document.createElement('button')
-    completedItems.textContent = 'Check'
-    completedItems.className = ('completed-btn')
-    todoItems.appendChild(completedItems)
     
     //deleted todo items
     let deletedItems = document.createElement('button')
-    deletedItems.textContent = 'Delete'
+    deletedItems.innerHTML = '&#10006'
     deletedItems.className = ('deleted-btn')
     todoItems.appendChild(deletedItems)
    
     list.appendChild(todoItems) 
 
-    todoInput.value = ""     
+    todoInput.value = ""    
+  }
+
+
+    
+      
+    
 }) 
+
+  
 
 list.addEventListener('click', (e)=>{
     // console.log(e.target);
@@ -41,10 +52,10 @@ list.addEventListener('click', (e)=>{
     }
     //completed
     if(item.classList[0] === 'completed-btn'){
-        let todo = item.parentElement
-        todo.classList.toggle('completed')
-      }
-
+      let todo = item.parentElement
+      todo.classList.toggle('completed')
+      console.log(todo);
+    }
 
 })
  
